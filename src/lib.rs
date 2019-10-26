53ead50c337332169da551c35893f4cf23e86bbd
Nicolas AMBRY <nicolas.ambry@gmail.com>
2019-10-26 19:58:34 +0200

    Changed crypto lib, added UI to display hashes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod utils;
-extern crate crypto;
+extern crate data_encoding;
+extern crate ring;
 
-use self::crypto::digest::Digest;
-use self::crypto::sha2::Sha256;
+use data_encoding::HEXLOWER;
+use ring::digest::{digest, SHA256};
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -11,12 +13,14 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-struct Block {
+#[wasm_bindgen]
+pub struct Block {
     id: u32,
     data: String,
     hash: String,
 }
 
+#[wasm_bindgen]
 impl Block {
     pub fn new() -> Block {
         let id = 0;
@@ -28,18 +32,36 @@ impl Block {
         }
     }
 
-    pub fn mine(&mut self) {
-        let mut hasher = Sha256::new();
-        let message = format!("{}{}", self.id, self.data);
+    pub fn set_id(&mut self, id: u32) {
+        self.id = id;
+    }
 
-        hasher.input_str(&message);
-        let hex = hasher.result_str();
-        self.hash = hex;
+    pub fn get_id(&self) -> u32 {
+        self.id
+    }
+
+    pub fn set_data(&mut self, data: String) {
+        self.data = data;
+    }
+
+    pub fn get_data(&self) -> String {
+        let data = self.data.clone();
+        data
+    }
+
+    pub fn get_hash(&self) -> String {
+        let hash = self.hash.clone();
+        hash
+    }
+
+    pub fn mine(&mut self) {
+        let message = format!("{}{}", self.id, self.data);
+        let digest = digest(&SHA256, &message.as_bytes());
+        self.hash = HEXLOWER.encode(digest.as_ref());
     }
 }
 
-fn main() {
-    let mut block: Block = Block::new();
-    block.mine();
-    println!("{}", block.hash);
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    set_panic_hook();
 }
--- www/index.js
+++ www/index.js
@@ -1,1 +1,29 @@
-import * as wasm from "blockchain-wasm";
+import { Block, init_panic_hook } from "blockchain-wasm";
+
+const id = document.getElementById("id");
+const data = document.getElementById("data");
+const hash = document.getElementById("hash");
+const mineBtn = document.getElementById("mine");
+
+const block = Block.new();
+
+function init() {
+  init_panic_hook();
+  id.value = block.get_id();
+  data.value = block.get_data();
+  hash.value = block.get_hash();
+}
+
+function update() {
+  id.value = block.get_id();
+  data.value = block.get_data();
+  hash.value = block.get_hash();
+}
+
+mineBtn.addEventListener("click", () => {
+  block.set_data(data.value);
+  block.mine();
+  update();
+});
+
+init();
