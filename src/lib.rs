009cb0cc81555189497d7634ab9b2d6c140af32b
Nicolas AMBRY <nicolas.ambry@gmail.com>
2019-10-26 14:04:04 +0200

    Added mine function to generate SHA256 hash

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod utils;
+extern crate crypto;
 
+use self::crypto::digest::Digest;
+use self::crypto::sha2::Sha256;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -10,6 +13,33 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 struct Block {
     id: u32,
+    data: String,
     hash: String,
-    previous: u32,
+}
+
+impl Block {
+    pub fn new() -> Block {
+        let id = 0;
+
+        Block {
+            id,
+            data: "test".to_string(),
+            hash: "".to_string(),
+        }
+    }
+
+    pub fn mine(&mut self) {
+        let mut hasher = Sha256::new();
+        let message = format!("{}{}", self.id, self.data);
+
+        hasher.input_str(&message);
+        let hex = hasher.result_str();
+        self.hash = hex;
+    }
+}
+
+fn main() {
+    let mut block: Block = Block::new();
+    block.mine();
+    println!("{}", block.hash);
 }
